c41cb9f247ca6aedb6af9e4bc672c70988fa726d
Vivek Singh <vivekonline01@gmail.com>
2018-12-23 00:29:06 +0530

    almost done

--- src/Box.js
+++ src/Box.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
 import styles from './Box.module.css';
-import gridDataSetFunc from './helper';
+import {gridDataSetFunc, traverseGrid} from './helper';
 
 class Box extends Component {
-  componentDidMount() {
-    window.addEventListener('keyup', (event) => {
-      const { key } = event;
-      if(key === 'ArrowUp' || key === 'ArrowDown' || key === 'ArrowLeft' || key === 'ArrowRight') {
-        let el = document.getElementById('grid').getElementsByTagName('div');
-        el[0].focus();
-        console.log(el);
-      }
-    });
-  }
+  state = {
+    currentEl: null,
+    found: 0,
+    moves: 0,
+    gameOver: false,
+    mushroomPositions: []
+  };
 
-  gridMaker = () => {
+  onFocusHandler = (event) => {
+    this.setState({
+      currentEl: event.target
+    });
+
+    if (event.target.dataset.value === '1' && !this.state.mushroomPositions.includes(event.target.dataset.position) && this.props.rows > this.state.found) {
+      this.setState({
+        found: this.state.found + 1, 
+        moves: this.state.moves + 1,
+        mushroomPositions: [...this.state.mushroomPositions, event.target.dataset.position] 
+      });
+    } else {
+      this.setState(prevState => {
+        return { moves: prevState.moves + 1 };
+      });
+    }
+    this.gameOver();
+  };
+
+  gameOver = () => {
+    if(this.state.found === this.props.rows) {
+      this.setState({
+        gameOver: true
+      });
+      document.removeEventListener('keyup', traverseGrid);
+    }
+  };
+
+  gridArrayMaker = () => {
     const {rows, cols} = this.props;
     const mushroomData = gridDataSetFunc(rows, cols);
     let array = [];
@@ -22,7 +47,7 @@ class Box extends Component {
     for(let i = 1; i <= rows; i++) {
       const arrInner = [];
       for(let j = 1; j <= cols; j++) {
-        arrInner.push(<div tabIndex='0' key={`${i}${j}`} data-position={`${i}${j}`} data-value={mushroomData[count]}></div>);
+        arrInner.push(<div onFocus={this.onFocusHandler} tabIndex='0' key={`${i}${j}`} data-position={`${i}-${j}`} data-value={mushroomData[count]}>{mushroomData[count]}</div>);
         count++;
       }
       array.push(arrInner);
@@ -30,13 +55,33 @@ class Box extends Component {
     return array;
   };
 
+  gridArray = this.gridArrayMaker();
+
   render() {
-    const gridElements = this.gridMaker();
+    const scorecard = <div className={styles.Scorecard}>You took {this.state.moves} moves to fill your tummy!</div>;
     return (
-      <div className={styles.Box} id="grid">
-        {gridElements.map((el, i) => <section key={i}>{el}</section>)}
+      <div>
+        {this.state.gameOver ? scorecard : null};
+        <div className={styles.Box} id="grid">
+          {this.gridArray.map((el, i) => <section key={i}>{el}</section>)}
+        </div>
       </div>
     );
+  }
+
+  componentDidMount() {
+    let gridElementArray = document.getElementById('grid').getElementsByTagName('div');
+
+    document.addEventListener('keyup', ({key}) => {
+      console.log('run run');
+      if(key === 'ArrowUp' || key === 'ArrowDown' || key === 'ArrowLeft' || key === 'ArrowRight') {
+        gridElementArray[0].focus();
+      } else {
+        alert('Use arrow keys to move Mario');
+      }
+    }, {capture: true, once: true});
+
+    document.addEventListener('keyup', (event) => traverseGrid(event.key, gridElementArray, this.state.currentEl, this.props))
   }
 }
 
--- src/helper.js
+++ src/helper.js
@@ -1,4 +1,7 @@
-function gridDataset(rows, cols) {
+/**
+ * Function to generate an array of random 0 & 1
+ */
+export const gridDataSetFunc = function(rows, cols) {
   let array = [];
   let count = 0;
   for(let i = 1; array.length < rows*cols; i++) {
@@ -10,19 +13,65 @@ function gridDataset(rows, cols) {
   return array;
 }
 
-export default gridDataset;
-
-/*function defaulter(rows, cols) {
-  const dataset = gridDataset(rows, cols);
-  let array = [];
-  let count = 0;
-  for(let i = 1; i <= rows; i++) {
-    const arrIn = [];
-    for(let j = 1; j <= cols; j++) {
-      arrIn.push(`div ${i}${j}--${dataset[count]}`);
-      count++;
-    }
-    array.push(arrIn);
+/**
+ * Event handler for "KEYUP"
+ */
+export const traverseGrid = (key, gridArray, grid, props) => {
+  const pos = grid.dataset.position.split('-');
+  const {rows, cols} = props;
+  switch(key) {
+    case 'ArrowUp': moveUp(pos, gridArray)
+      break;
+    case 'ArrowDown': moveDown(pos, gridArray, rows)
+      break;
+    case 'ArrowLeft': moveLeft(pos, gridArray)
+      break;
+    case 'ArrowRight': moveRight(pos, gridArray, cols)
+      break;
+    default: alert('Use arrow keys to feed Mario');
   }
-  return array;
-} */
+};
+
+/** 
+ * Logic for traversing through the grids
+ */
+
+const moveDown = (pos, gridArray, rows) => {
+  if(+pos[0] === rows) moveUp(pos, gridArray);
+  const gridId = `${(parseInt(pos[0]) + 1)}-${pos[1]}`; 
+  for(let i = 0; i < gridArray.length; i++) {
+    if(gridArray[i].dataset.position === gridId) {
+      gridArray[i].focus();
+    }
+  }
+};
+
+const moveUp = (pos, gridArray) => {
+  if(+pos[0] === 1) moveDown(pos, gridArray);
+  const gridId = `${(parseInt(pos[0]) - 1)}-${pos[1]}`; 
+  for(let i = 0; i < gridArray.length; i++) {
+    if(gridArray[i].dataset.position === gridId) {
+      gridArray[i].focus();
+    }
+  }
+};
+
+const moveLeft = (pos, gridArray) => {
+  if(+pos[1] === 1) moveRight(pos, gridArray);
+  const gridId = `${pos[0]}-${(parseInt(pos[1]) - 1)}`;
+  for(let i=0; i<gridArray.length; i++) {
+    if(gridArray[i].dataset.position === gridId) {
+      gridArray[i].focus();
+    }
+  }
+};
+
+const moveRight = (pos, gridArray, cols) => {
+  if(+pos[1] === cols) moveLeft(pos, gridArray);
+  const gridId = `${pos[0]}-${(parseInt(pos[1]) + 1)}`;
+  for(let i=0; i<gridArray.length; i++) {
+    if(gridArray[i].dataset.position === gridId) {
+      gridArray[i].focus();
+    }
+  }
+};
